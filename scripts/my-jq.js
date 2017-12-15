a8711c0624e753fe0ef3625fa5478b348c8c9343
bond619 <31978635+bond619@users.noreply.github.com>
2017-12-15 05:05:20 -0600

    Update my-jq.js

--- scripts/my-jq.js
+++ scripts/my-jq.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-    $("p").text("The DOM is now loaded and can be manipulated.");
-});
+    $( document ).ready(function() {
+        console.log( "document loaded" );
+    });
+ 
+    $( window ).on( "load", function() {
+        console.log( "window loaded" );
+    });
